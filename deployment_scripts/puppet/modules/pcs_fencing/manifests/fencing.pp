1e21a2251ad03bec546f4c9223ce04ce3b445940
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-04-17 14:01:13 +0200

    Add license headers and docs for parameters
    Change-Id: Id574e79d21239d4a6f3954d101f0cced66dd4073
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment_scripts/puppet/modules/pcs_fencing/manifests/fencing.pp
+++ deployment_scripts/puppet/modules/pcs_fencing/manifests/fencing.pp
@@ -1,6 +1,38 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
 # == Define: pcs_fencing::fencing
 #
-# Configure STONITH resources for corosync/pacemaker.
+# Configure STONITH resources for Pacemaker.
+#
+# === Parameters:
+#
+# [*agent_type*]
+#   The fence agent name for a STONITH resource
+#   Defaults to undef
+#
+# [*parameters*]
+#   The hash of parameters for a STONITH resource
+#   Defaults to False
+#
+# [*operations*]
+#   The hash of operations for a STONITH resource
+#   Defaults to False
+#
+# [*meta*]
+#   The hash of metadata for a STONITH resource
+#   Defaults to False
 #
 define pcs_fencing::fencing (
   $agent_type,
