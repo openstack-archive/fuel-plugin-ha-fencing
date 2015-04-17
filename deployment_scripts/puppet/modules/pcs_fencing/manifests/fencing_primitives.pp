df786b613ee3ba76b15f2d2901126f56e7fedd51
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-04-17 14:01:13 +0200

    Add license headers and docs for parameters
    Change-Id: Id574e79d21239d4a6f3954d101f0cced66dd4073
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- deployment_scripts/puppet/modules/pcs_fencing/manifests/fencing_primitives.pp
+++ deployment_scripts/puppet/modules/pcs_fencing/manifests/fencing_primitives.pp
@@ -1,5 +1,34 @@
-# Creates fencing primitives and topology for given nodes.
-# Assumes all nodes have the same OS installed
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# == Class: pcs_fencing::fencing_primitives
+#
+# Creates Pacemaker fencing primitives and topology for given nodes.
+# Assumes all nodes have the same OS installed.
+#
+# === Parameters:
+#
+# [*fence_primitives*]
+#   The hash of parameters for STONITH resources in Pacemaker
+#   Defaults to undef
+#
+# [*fence_topology*]
+#   The hash of parameters for a fencing topology in Pacemaker
+#   Defaults to undef
+#
+# [*node*]
+#   The array of node names in Pacemaker cluster
 #
 class pcs_fencing::fencing_primitives (
   $fence_primitives,
